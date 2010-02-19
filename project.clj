e64af06aebdc5d7f9230e44c79ef7db290025824
lance bradley <l@ncebradley.org>
2010-02-18 20:21:50 -0800

    lein deps test

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject json-test "0.1.0-SNAPSHOT"
+  :description "json-test"
+  :url "http://github.com/lancepantz/json-test"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [clj-json "0.1.0-SNAPSHOT"]
+                ])
---
+++ src/blah.clj
@@ -0,0 +1,2 @@
+(use 'clj-json)
+(println (generate-string {"foo" "bar"}))
